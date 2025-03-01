1afd9b7ad41ea321d68c6d03da58b0f116bc175e
Your Name <you@example.com>
2025-03-01 18:32:53 +0800

    02

--- install.sh
+++ install.sh
@@ -9,9 +9,6 @@ rm -rf ktransformers/ktransformers_ext/cuda/build
 rm -rf ktransformers/ktransformers_ext/cuda/dist
 rm -rf ktransformers/ktransformers_ext/cuda/*.egg-info
 
-echo "Installing python dependencies from requirements.txt"
-pip install -r requirements-local_chat.txt
-
 # 设置CUDA架构标志 - 支持P100(sm_60)和RTX 4060(sm_89)
 export TORCH_CUDA_ARCH_LIST="6.0;8.9"
 
